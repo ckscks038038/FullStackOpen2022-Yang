fe75f737f158e8df64f6778d97f05440e8db177e
ckscks038038 <ckscks038038@gmail.com>
2022-07-26 12:32:53 +0800

    passing an array into components, and receiving the array by argument props; learning the implementation of array method forEach

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -6,36 +6,52 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
+  let res = [];
+  props.parts.forEach((ele) => {
+    res.push(<p>{ele.name} {ele.exercises}</p>)
+  });
+
   return (
     <>
-      <p>{props.part} {props.exercise}</p>
+      {res}
     </>
   )
 }
 const Total = (props) => {
+  let res = 0;
+  props.parts.forEach((ele) => {
+    res += ele.exercises
+  })
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {res}</p>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
+  
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
