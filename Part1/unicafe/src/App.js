d5d0bde39b8c7e0fafb7c8ff46e5dc0bed05f07d
ckscks038038 <ckscks038038@gmail.com>
2022-07-29 17:32:06 +0800

    unicafe-step6, final version

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>
-}
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
 const StatisticLine = (props) => {
   return (
@@ -37,6 +35,7 @@ const Statistics = ({good, neutral, bad}) => {
     </>
   )
 }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
