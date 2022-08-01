32f61c7910611ae629e543075e14126b3fffb38a
ckscks038038 <ckscks038038@gmail.com>
2022-08-01 16:59:01 +0800

    phonebook step3

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number:123 } ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject ={ name: newName }
+    const personObject ={ name: newName, number: newNumber}
     // go through the persons array to check 
     // if there is an object within the array identical to "personObject"
     // if true, noitfy user with alert
@@ -20,12 +19,15 @@ const App = () => {
     }else {
       setPersons(persons.concat(personObject))
     }
-
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     return setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    return setNewNumber(event.target.value)
   }
   
   return (
@@ -38,13 +40,17 @@ const App = () => {
                   />
         </div>
         <div>
+          number: <input value={newNumber}
+                 onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
